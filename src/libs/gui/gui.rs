865eb4954911798b59d8ece5ca946e8d2d8cf4a1
Matt Gumbley <matt.gumbley@devzendo.org>
2023-07-24 08:48:52 +0100

    Extract function.

--- src/libs/gui/gui.rs
+++ src/libs/gui/gui.rs
@@ -332,6 +332,13 @@ impl Gui {
         gui
     }
 
+    fn draw_meter_line(theta: f32, long_r: f32, short_r: f32, mid_x: i32, mid_y: i32) {
+        draw_line(mid_x - (long_r * theta.cos()) as i32,
+                  mid_y - (long_r * theta.sin()) as i32,
+                  mid_x - (short_r * theta.cos()) as i32,
+                  mid_y - (short_r * theta.sin()) as i32);
+    }
+
     fn draw_meter(wid: &mut Widget, background: Color, signal_strength: f32) {
         push_clip(wid.x(), wid.y(), wid.width(), wid.height());
         draw_rect_fill(wid.x(), wid.y(), wid.width(), wid.height(), background);
@@ -345,6 +352,7 @@ impl Gui {
         // The meter is anchored at..
         let mid_x = wid.width() / 2;
         let mid_y = (wid.height() / 4) * 3;
+
         // Draw arc
         let arc_radius: f32 = 110.0;
         let mut arc_theta: f32 = 0.7854;
@@ -356,16 +364,14 @@ impl Gui {
                       mid_y - (arc_radius * (arc_theta + delta_theta).sin()) as i32);
             arc_theta += delta_theta;
         }
+
         // Draw needle
         let theta = signal_strength * 1.5708 + 0.7854;
         let long_r = 120.0;
         let short_r = 40.0;
-        draw_line(mid_x - (long_r * theta.cos()) as i32,
-                  mid_y - (long_r * theta.sin()) as i32,
-                  mid_x - (short_r * theta.cos()) as i32,
-                  mid_y - (short_r * theta.sin()) as i32);
-
         info!("Updating meter to theta {} signal strength is {}", theta, signal_strength);
+        Self::draw_meter_line(theta, long_r, short_r, mid_x, mid_y);
+
         set_line_style(LineStyle::Solid, 0); // reset it, or everything is thick
         pop_clip();
     }
